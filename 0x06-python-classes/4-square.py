1bb0465072b45c41b6f772ecb9bc6c38c90d5165
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-06-28 07:28:58 -0700

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class that defines a square
+"""
+
+
+class Square:
+    """
+    class of squares.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
