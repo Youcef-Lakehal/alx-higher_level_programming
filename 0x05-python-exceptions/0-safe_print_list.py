46e1b7603173ae85a2fa3053032f6bfc064ac445
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-06-28 06:55:12 -0700

    Correction 2 task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end='')
-        print()
-        return i + 1
+            print("{}".format(my_list[i]), end='')
+        print("\n", end='')
+        return (i + 1)
     except IndexError:
         print("\n", end='')
-        return i
-
+        return (i)
